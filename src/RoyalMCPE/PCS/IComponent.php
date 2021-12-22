a5156154ed2ff7274ae63a21e2619ba33c0264ac
RoyalMCPE <coolestgamer117@gmail.com>
2021-12-21 22:57:34 -0700

    Reset

--- src/RoyalMCPE/PCS/IComponent.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace RoyalMCPE\PCS;
-
-interface IComponent {}
--- src/RoyalMCPE/PCS/exceptions/ComponentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace RoyalMCPE\PCS\exceptions;
-
-use Exception;
-
-class ComponentException extends Exception {}
